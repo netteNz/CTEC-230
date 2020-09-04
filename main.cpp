cfad1fe15eef7029e6cbed3f66ada6482624b4b0
Emanuel Lugo <lugo.emanuel@gmail.com>
2020-09-03 22:42:45 -0400

    first assignment

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Emanuel Lugo
+//S00821527
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Emanuel Lugo Rivera"
+		 << "\n2504, Parque San Antonio II"
+		 << "\nCaguas, PR 00727";
+	return 0;
+}
+
